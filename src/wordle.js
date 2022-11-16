71b664e11c74341cd63ce0ddce9ff2dd263fdf4c
Natiel Traore <you@example.com>
2022-11-17 00:09:32 +0200

    More style adjustments, need to implement validate func

---
+++ src/wordle.js
@@ -0,0 +1,25 @@
+
+
+
+// document.addEventListener('DOMContentLoaded', initalFocus)
+const inputArr =  document.getElementsByClassName('ur-input')
+const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+for (let i = 0; i < inputArr.length; i++) {
+    const ipt = array[i];
+    ipt.addEventListener('DOMContentLoaded', validate)}
+
+
+const validate = function()  {
+        // input.textContent in abc === false) {
+        console.log(input.textContent)
+        input.innerHTML = ''
+        }
+    // }
+
+// abc.forEach(e => {
+//     console.log(e)
+// });
+
+
+    
