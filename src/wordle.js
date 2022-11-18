328555f9ad384c964ded811947d1accd73b541e2
Natiel Traore <you@example.com>
2022-11-18 02:18:32 +0200

    still need to figure out apllication pt

--- src/wordle.js
+++ src/wordle.js
@@ -5,17 +5,29 @@
 const inputArr =  document.getElementsByClassName('ur-input')
 const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+
+let f = inputArr.item(0)
+console.log(f)
+document.g
+
 for (let i = 0; i < inputArr.length; i++) {
-    const ipt = array[i];
-    ipt.addEventListener('DOMContentLoaded', validate)}
+    const e = inputArr[i];
+    e.addEventListner('click', () => {alert('hi')})
+
+}
+
+// for (let i = 0; i < inputArr.length; i++) {
+//     let ipt = array[i];
+//     console.log(ipt)
+//     ipt.addEventListener('click', validate)}
 
 
-const validate = function()  {
-        // input.textContent in abc === false) {
-        console.log(input.textContent)
-        input.innerHTML = ''
-        }
-    // }
+const validate = function(ipt)  {
+    console.log(hello)
+    if ('abc'.indexOf(ipt.innerHTML) > -1) {
+        ipt.innerHTML = 'h';
+    } 
+    }
 
 // abc.forEach(e => {
 //     console.log(e)
