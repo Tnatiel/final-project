a25c1bcc258907c8b7082e9b14bf1da494a58dbd
Natiel Traore <you@example.com>
2022-11-19 07:37:55 +0200

    made a var to hold curr guess, still need to connect keyboard

--- src/wordle.js
+++ src/wordle.js
@@ -3,12 +3,14 @@ const buttons = document.getElementsByClassName('kbd-btn')
 const input1 = document.getElementById('0-0')
 let row = 0
 let col = 0
+let curGuess = ''
 let gameOver = false
 const events = ['onkeyup', 'click']
 abc ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 // Focus on first cell on page load
 document.addEventListener('DOMcontentloaded', foucsCell(input1))
+
 
 function foucsCell() {
     if (row > 5) {
@@ -26,11 +28,15 @@ const handleClick = (key) => {
     addLetter(key);
 }
 
+const handleKey = (key) => {
+    addLetter(key)
+}
+
 function addLetter(letter){
     const curCell = document.getElementById(`${row}-${col}`);
-    
-   
     if (abc.includes(letter)) {
+        curGuess += letter;
+        console.log(curGuess)
         curCell.textContent = letter;
         unFocusCell();
         col++;
@@ -39,23 +45,23 @@ function addLetter(letter){
     } else if (letter == 'Backspace' || letter == 'del') {
         if (col > 0) {
             unFocusCell();
+            curGuess = curGuess.substring(0, curGuess.length - 1);
             col--;
             document.getElementById(`${row}-${col}`).textContent = '';
-            nextCell()
+            nextCell();
         }
     } 
 }
 
 function nextCell() {
     if (row > 5) {
-        alert('done')
+        alert('done');
         gameOver = true;
     } else if (col > 4) {
         row++;
         col = 0
         foucsCell();
     } else {
-        console.log(' 5')
         foucsCell();
     }
 }
